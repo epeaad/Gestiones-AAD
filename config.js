9c8d300c7e716609bdfa355eed4a9fe84ce420fb
epeaad <epeaad@gmail.com>
2026-08-17 08:40:01 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -6,4 +6,4 @@
 // Tiene esta forma:
 // https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXXXXX/exec
 
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz9VyqNm0NHwzCBYysIgexPzZW75yjKlxNE4_QzfMuCeGuUPSdXxB3D3__BH2ANXoAi/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwDJcAW2icihVgU0nFp2MZeJIXtOqs_FPb9ENs0dpQ5kvdAYB9J8h2ejVL939OV5DEi/exec";
